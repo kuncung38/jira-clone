df6f6140759c88f932e1f94b6fc9606077f5088c
Kuncung <rickowijaya@gmail.com>
2024-10-18 02:23:56 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant={"destructive"} size={"sm"}>
+        Click Me
+      </Button>
+    </div>
+  );
+}
